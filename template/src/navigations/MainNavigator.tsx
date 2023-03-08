d42c9a4623929ed191c0957413a7d3feb0fe183f
Hritik Singh <48889727+hrithik73@users.noreply.github.com>
2023-03-08 19:48:17 +0530

    feat: running good on ios

--- template/src/navigations/MainNavigator.tsx
+++ template/src/navigations/MainNavigator.tsx
@@ -1,13 +1,24 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { screenName } from '@src/constants/screen';
 import Login from '@src/screens/auth/Login';
+import SingUp from '@src/screens/auth/SignUp';
 
 const Auth = createNativeStackNavigator();
 
 const AuthNavigator = () => {
   return (
     <Auth.Navigator>
-      <Auth.Screen name="Login" component={Login} />
+      <Auth.Screen
+        name={screenName.login.name}
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Auth.Screen
+        name={screenName.signUp.name}
+        component={SingUp}
+        options={{ headerShown: false }}
+      />
     </Auth.Navigator>
   );
 };
---
+++ template/src/screens/auth/SignUp.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+const SingUp = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+export default SingUp;
